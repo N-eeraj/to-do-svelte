52414abb2e7d99ec4284ab5a2543ecdf6bcecee1
N-eeraj <neeraj7rajeena@gmail.com>
2023-06-25 00:07:29 +0530

    deployment setup

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/to-do-svelte/' : '/',
   plugins: [svelte()],
 })
